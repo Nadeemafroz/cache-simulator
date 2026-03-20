dc736cb778d263b8d4ba9dddcb558544f7329d59
mohammed nadeem afroz <132338163+Nadeemafroz@users.noreply.github.com>
2026-03-20 17:17:39 +0530

    Update CacheController.java

--- backend/src/main/java/com/systemdesign/cache_simulator/controller/CacheController.java
+++ backend/src/main/java/com/systemdesign/cache_simulator/controller/CacheController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/cache")
 public class CacheController {
